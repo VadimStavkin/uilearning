0e0d8c5ee53810fe8bee4fa9ef54e91031fe4104
Vadim Stavkin <vstavkin@griddynamics.com>
2018-11-28 20:10:25 +0400

    Login modal - fixed issues #2

--- components/LoginModal/LoginModal.js
+++ components/LoginModal/LoginModal.js
@@ -1,14 +1,6 @@
 'use strict'
 const currentDocument = document.currentScript.ownerDocument;
 
-const loginDefaultClass = 'login-modal-login-icon-inactive';
-const loginInvalidClass = 'login-modal-login-icon-invalid';
-const loginActiveClass = 'login-modal-login-icon-active';
-
-const passwordDefaultClass = 'login-modal-password-icon-inactive';
-const passwordInvalidClass = 'login-modal-password-icon-invalid';
-const passwordActiveClass = 'login-modal-password-icon-active';
-
 
 class LoginModal extends HTMLElement {
   constructor() {
@@ -36,107 +28,7 @@ class LoginModal extends HTMLElement {
       e.stopPropagation();
     });
 
-    let loginElem = this.shadowRoot.querySelector('.login-modal-login');
-    loginElem.addEventListener('click', e =>{
-      this.setLoginIconInvalid();
-      e.stopPropagation();
-    });
-    loginElem.addEventListener('keyup', e =>{
-      this.setLoginIconInvalid();
-    });
-    loginElem.addEventListener('focus', e =>{
-      this.setLoginIconOnFocus();
-    });
-    loginElem.addEventListener('blur', e =>{
-      this.setLoginIconOnBlur();
-    });
-
-    let passwordElem = this.shadowRoot.querySelector('.login-modal-password');
-    passwordElem.addEventListener('click', e =>{
-      this.setPasswordIconInvalid();
-      e.stopPropagation();
-    });
-    passwordElem.addEventListener('keyup', e =>{
-      this.setPasswordIconInvalid();
-    });
-    passwordElem.addEventListener('focus', e =>{
-      this.setPasswordIconOnFocus();
-    });
-    passwordElem.addEventListener('blur', e =>{
-      this.setPasswordIconOnBlur();
-    });
-
   }
-  setLoginIconInvalid() {
-    let loginElem = this.shadowRoot.querySelector('.login-modal-login');
-    let loginIconElem = this.shadowRoot.querySelector('.login-modal-login-icon');
-
-    if(loginElem.checkValidity() === false){
-      loginIconElem.classList.add(loginInvalidClass);
-    }
-    else{
-      loginIconElem.classList.remove(loginInvalidClass);
-    }
-  }
-  setLoginIconOnFocus(){
-    let loginElem = this.shadowRoot.querySelector('.login-modal-login');
-    let loginIconElem = this.shadowRoot.querySelector('.login-modal-login-icon');
-
-    if(loginElem.checkValidity() === false){
-      loginIconElem.classList.add(loginInvalidClass);
-    }
-    else{
-      loginIconElem.classList.remove(loginInvalidClass);
-    }
-
-    loginIconElem.classList.add(loginActiveClass);
-    loginIconElem.classList.remove(loginDefaultClass);
-  }
-  setLoginIconOnBlur(){
-    let loginElem = this.shadowRoot.querySelector('.login-modal-login');
-    let loginIconElem = this.shadowRoot.querySelector('.login-modal-login-icon');
-
-    loginIconElem.classList.remove(loginInvalidClass);
-
-    loginIconElem.classList.add(loginDefaultClass);
-    loginIconElem.classList.remove(loginActiveClass);
-  }
-
-  setPasswordIconInvalid() {
-    let passwordElem = this.shadowRoot.querySelector('.login-modal-password');
-    let passwordIconElem = this.shadowRoot.querySelector('.login-modal-password-icon');
-
-    if(passwordElem.checkValidity() === false){
-      passwordIconElem.classList.add(passwordInvalidClass);
-    }
-    else{
-      passwordIconElem.classList.remove(passwordInvalidClass);
-    }
-  }
-  setPasswordIconOnFocus(){
-    let passwordElem = this.shadowRoot.querySelector('.login-modal-password');
-    let passwordIconElem = this.shadowRoot.querySelector('.login-modal-password-icon');
-
-    if(passwordElem.checkValidity() === false){
-      passwordIconElem.classList.add(passwordInvalidClass);
-    }
-    else{
-      passwordIconElem.classList.remove(passwordInvalidClass);
-    }
-
-    passwordIconElem.classList.add(passwordActiveClass);
-    passwordIconElem.classList.remove(passwordDefaultClass);
-  }
-  setPasswordIconOnBlur(){
-    let passwordElem = this.shadowRoot.querySelector('.login-modal-password');
-    let passwordIconElem = this.shadowRoot.querySelector('.login-modal-password-icon');
-
-    passwordIconElem.classList.remove(passwordInvalidClass);
-
-    passwordIconElem.classList.add(passwordDefaultClass);
-    passwordIconElem.classList.remove(passwordActiveClass);
-  }
-  
   openModal() {
     let backgroundElem = this.shadowRoot.querySelector('.login-modal-popup-background');
     if(backgroundElem.classList.contains('hidden')){
